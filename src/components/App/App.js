5bd9c653e9f6b6f4059e1fed1349c2930d81f98c
Luiz Filipe <moresco.luiz@gmail.com>
2018-08-04 13:48:10 -0300

    Connect the App component with reducer to show the person list

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'unistore/react';
+import { props, actions } from '../../reducer';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount () {
+    const { getPersonList } = this.props;
+    getPersonList();
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    const { personList } = this.props;
+
+    return(
+      <div>
+        {personList &&
+          personList.map(person => (
+            <p key={person.id} >{person.name}</p>
+          ))
+        }
       </div>
     );
   }
 }
 
-export default App;
+export default connect(props, actions)(App);
