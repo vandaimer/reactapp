84dc877ffb8463269b6f25bf70186c0cc028be57
Luiz Filipe <moresco.luiz@gmail.com>
2018-08-04 15:04:58 -0300

    Improve reducer to return a empty list if get an error

--- src/reducer.js
+++ src/reducer.js
@@ -1,19 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
 const initialState = {
-  personList: [],
+  personList: []
 };
 
 const actions = store => ({
   async getPersonList(state) {
-    const url = 'http://localhost:3000/api/person';
-    const { data: { items: personList } } = await axios.get(url);
-    return { personList };
-  },
+    const url = "http://localhost:3000/api/person";
+    try {
+      const {
+        data: { items: personList }
+      } = await axios.get(url);
+      return { personList };
+    } catch (e) {
+      const { personList } = state;
+      return { personList };
+    }
+  }
 });
 
-
-const props = [ 'personList' ];
-
+const props = ["personList"];
 
 export { initialState, props, actions };
